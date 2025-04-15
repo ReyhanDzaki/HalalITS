ada32e8dba15049008db3cd0d6c03463879f9a97
Reyhan Dzaki Abdillah <reyhan.dzaki.a@gmail.com>
2025-04-15 10:44:35 +0700

    update table migrations naming

--- database/migrations/2025_04_15_000000_create_umkms_and_photos_tables.php.php
+++ database/migrations/2025_04_15_000000_create_umkms_and_photos_tables.php.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTables extends Migration
+class CreateUmkmsAndPhotosTables extends Migration
 {
     public function up()
     {
