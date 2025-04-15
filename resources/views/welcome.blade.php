6fdbf0d1dd5f88de995505722d201cbf6175105a
Reyhan Dzaki Abdillah <reyhan.dzaki.a@gmail.com>
2025-04-15 11:20:02 +0700

    fix mix laravel dependancy crash

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <title>Halal ITS</title>
     @livewireStyles
